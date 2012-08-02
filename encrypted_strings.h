2e1f64e8ca1512321d84e1bf9de25c0f88d88539
Kerrigan29a <kerrigan29a@gmail.com>
2012-08-02 23:13:40 +0200

    Some minore fixes

--- encrypted_strings.h
+++ encrypted_strings.h
@@ -15,34 +15,33 @@ extern "C" {
 #include <assert.h>
 #endif
 
+
 /*
  * Export the variables related to the encrypted text strings
  */
 #define STR(id, len, ...) \
-	extern char es_##id[]; \
-	extern unsigned es_len_##id;
+extern char es_##id[]; \
+extern unsigned es_len_##id;
 #include "encrypted_strings.def"
 #undef STR
 
 #ifndef __cplusplus
-#define ES_CREATE_BUFFER(id, buffer) \
-	char buffer[es_len_##id]
-#endif
 
-#ifdef __cplusplus
+#define ES_CREATE_BUFFER(id, buffer) char buffer[es_len_##id]
 
-#define ES_DECRYPT(id, buffer) \
-	char buffer[es_len_##id]; \
-	_es_decrypt(es_##id, es_len_##id, buffer)
+#define ES_DECRYPT(id, buffer) do { \
+	assert(buffer != NULL); \
+	_es_decrypt(es_##id, es_len_##id, buffer); \
+} while (0 /* CONST COND */)
 
 #else
 
-#define ES_DECRYPT(id, buffer) do { \
-		assert(buffer != NULL); \
-		_es_decrypt(es_##id, es_len_##id, buffer); \
-	} while (0 /* CONST COND */)
+#define ES_DECRYPT(id, buffer) \
+char buffer[es_len_##id]; \
+_es_decrypt(es_##id, es_len_##id, buffer)
 
 #endif
+
 
 char * _es_decrypt(char encrypted_string[], unsigned len, char *buffer);
 
